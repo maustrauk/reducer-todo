4d22c6ee69e6eaa4f900158094ceb8217f8c76bb
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-12-07 14:59:50 -0500

    Set up state in your component

---
+++ todo/src/components/TodoList.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Todo from "./Todo";
+
+const TodoList = props => {
+
+  return (
+    <div className="todo-list">
+      {props.todo.map(item => (
+        <Todo key={item.id} item={item} />
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
--- todo/src/reducers/reducer.js
+++ todo/src/reducers/reducer.js
@@ -1,10 +1,10 @@
 import {} from './../actions/reducerActions';
 
-export const initState = {
-  item: 'Learn about reducers',
+export const initState = [{
+  task: 'Learn about reducers',
   completed: false,
   id: 3892987589
-}
+}]
 
 const reducer = (state, action) => {
     switch (action.type) {
