5ecee6869b7cb9293bba9ca607a31f394bed1135
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-12-07 14:32:48 -0500

    Build a simple reducer and initial state

---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,18 @@
+import {} from './../actions/reducerActions';
+
+export const initState = {
+  item: 'Learn about reducers',
+  completed: false,
+  id: 3892987589
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+      
+
+      default:
+        return state
+    }
+  }
+
+export default reducer;
